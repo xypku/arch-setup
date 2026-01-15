69adbef0b3bd290a94fcc57e2f57a5805b88a075
shorinkiwata <fcl709@outlook.com>
2026-01-15 16:12:33 +0900

    menu style

--- install.sh
+++ install.sh
@@ -91,8 +91,8 @@ select_desktop() {
         "Shorin's Niri |niri"
         "KDE Plasma |kde"
         "GNOME |gnome"
-        "Quickshell(Hyprland)--illogical-impulse(end4) |end4"
-        "Quickshell(Niri or Hyprland)--DankMaterialShell(DMS)  |dms"
+        "illogical-impulse(end4)--Quickshell(Hyprland)|end4"
+        "DankMaterialShell(DMS)--Quickshell(Niri or Hyprland)|dms"
     )
     
     # 2. 绘制菜单 (半开放式风格)
